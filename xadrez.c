da5eb4824a2147f1b8db9afa0b3e22b36d56512c
maiconjesus21 <maiconzica289@gmail.com>
2025-03-10 02:45:27 +0000

    adicionei cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-   int main(){
-   
-    printf("novo commit\n");
 
-   int torre = 1;     //definição das variaveis
+
+
+int main(){
+
+   int torre = 1;
    int bispo = 1;
+   int movcavalo2 = 0;
 
-    printf("\n***movimento torre***\n"); //movimento da torre-while
+    printf("\n***movimento torre***\n");  //movimento da torre
    while (torre <= 5)
     {
-        printf("\ntorre: direita\n");
+        printf("\ntorre: direita\n");     
         torre++;
     }
-      printf("\n***movimento do bispo***\n");  // movimento do bispo do-while
+      printf("\n***movimento do bispo***\n"); //movimento do bispo
     
      do
      {
@@ -21,16 +23,26 @@
        bispo++;
     } while (bispo <= 5);
      
-    printf("\n***movimento da rainha***\n");  //movimento da rainha-for
+    printf("\n***movimento da rainha***\n"); //movimento da rainha
 
     for ( int rainha = 0; rainha < 8; rainha++)
     {
         printf("\nrainha: esquerda\n");
     }
     
+     printf("\n\n***movimento do cavalo***\n\n");   //movimento do cavalo
+   
+     for  (int movcavalo = 0; movcavalo < 1; movcavalo++)
+     {
+        printf("\ncavalo: baixo\n");
+        printf("\ncavalo: baixo\n");
+        while (movcavalo2 < 1)
+        {
+        printf("\ncavalo: esquerda\n");
+        movcavalo2++;
+        }
+    }
     return 0;
-    
-    
 }
 
 
